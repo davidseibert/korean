c7070636cdcee99f95f85a1085894de62efe56b7
davidseibert <info@davidseibert.me>
2016-05-14 12:23:23 -0400

    play with drawing

--- drawing.py
+++ drawing.py
@@ -1,5 +1,8 @@
 import treeprettyprinter
 from nltk.tree import Tree
+from StringIO import StringIO
+import sys
+import pydoc
 
 markets = Tree('markets', ['financial'])
 economic = Tree('economic', [])
@@ -28,4 +31,19 @@ had = Tree('had', [sbj(news), obj(effect)])
 
 sentence = had
 
+
+old_stdout = sys.stdout
+
+result = StringIO()
+
+sys.stdout = result
 sentence.pretty_print(ansi=True, unicodelines=True, nodecolor='red', leafcolor='blue')
+
+sys.stdout = old_stdout
+
+tree = result.getvalue()
+tree = tree.encode('utf-8')
+text = '... some text ... '
+#pydoc.pager(tree * 10)
+print tree * 10
+
