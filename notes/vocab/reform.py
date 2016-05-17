c9a5b4ea9d531df72696419b8d54887fcd86d138
David Seibert <pdavidseibert@gmail.com>
2016-05-16 23:23:37 -0400

    almost done

--- notes/vocab/reform.py
+++ notes/vocab/reform.py
@@ -24,6 +24,9 @@ class Card(object):
     def __repr__(self):
         format_string = '{0.rank}. {0.question} : {0.answer} ({0.age_repr}) [{0.quotient}]'
         return format_string.format(self)
+    
+    def present(self):
+        print self.question
 
 class Deck(object):
     def __init__(self):
@@ -32,8 +35,8 @@ class Deck(object):
 class Collection(object):
     def __init__(self, sourcefile_path):
         self.sourcefile_path = sourcefile_path
-        self.read_raw_cards()
         self.cards = []
+        self.read_raw_cards()
 
     def read_raw_cards(self):
         try:
@@ -42,7 +45,7 @@ class Collection(object):
                     raw_card = next(sourcefile)
                     self.process_raw_card(raw_card)
         except StopIteration:
-            print 'Done!'
+            pass
 
     def add_card(self, card):
         self.cards.append(card)
@@ -51,32 +54,36 @@ class Collection(object):
         unicode_card = raw_card.decode('utf-8')
         stripped = unicode_card.strip()
         fields = stripped.split('\t')
-        rank, quotient, last_reviewed, question, answer = tuple(fields)
+        rank, quotient, last_reviewed, answer, question = tuple(fields)
         last_reviewed_obj = datetime.strptime(last_reviewed, DATE_FORMAT)
         now = datetime.now()
         age = now - last_reviewed_obj
         card = Card(rank, quotient, age, question, answer)
-        print repr(card)
+        self.add_card(card)
 
 class Coach(object):
     def __init__(self):
         self.collections = []
 
 class Session(object):
-    def __init__(self):
-        pass
+    def __init__(self, collection_name):
+        self.collection = Collection(collection_name + '.tsv')
     
     def get_date(self):
         now = datetime.now()
         print now.strftime(DATE_FORMAT)
+        
+    def start(self):
+        for card in self.collection.cards:
+            card.present()
 
 
 
 def main():
-    top10 = Collection('top10.tsv')
+    session = Session('top10')
+    session.start()
 
 if __name__ == '__main__':
     main()
 
     
-
