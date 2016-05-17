59023336ac99603ed8fd1faaacdcb006a8d417e4
davidseibert <info@davidseibert.me>
2016-05-16 22:53:33 -0400

    starting flash card module

--- notes/vocab/reform.py
+++ notes/vocab/reform.py
@@ -1,31 +1,79 @@
 # coding 'utf-8'
 
-max_length = 0
+DATE_FORMAT = '%Y-%m-%d %H:%M:%S'
 
-def process_line(raw_line):
-    stripped = raw_line.strip()
-    return stripped 
+from datetime import datetime
 
-def check_len(line):
-    if len(line) > max_length: 
-        max_length = len(line)
-    return line
-        
- 
-def read_three_lines_at_a_time(sourcefile_path):
-    try:
-        with open(sourcefile_path) as sourcefile:
-            while True:
-                idx = process_line(next(sourcefile))
-                ko = check_len(process_line(next(sourcefile)))
-                en = process_line(next(sourcefile))
-                print idx, ko, en
-    except StopIteration:
+class Card(object):
+    def __init__(self, rank, quotient, age, question, answer):
+        self.rank = rank
+        self.quotient = quotient
+        self.age = age
+        self.question = question.encode('utf-8')
+        self.answer = answer
+
+    @property
+    def age_repr(self):
+        seconds = self.age.seconds
+        hours, remainder = divmod(seconds, 3600)
+        minutes, remainder = divmod(hours, 60)
+        hours_repr = str(hours) + 'h'
+        minutes_repr = str(minutes) + 'm'
+        return hours_repr + ' ' + minutes_repr
+
+    def __repr__(self):
+        format_string = '{0.rank}. {0.question} : {0.answer} ({0.age_repr}) [{0.quotient}]'
+        return format_string.format(self)
+
+class Deck(object):
+    def __init__(self):
+        self.cards = []
+
+class Collection(object):
+    def __init__(self, sourcefile_path):
+        self.sourcefile_path = sourcefile_path
+        self.read_raw_cards()
+        self.cards = []
+
+    def read_raw_cards(self):
+        try:
+            with open(self.sourcefile_path) as sourcefile:
+                while True:
+                    raw_card = next(sourcefile)
+                    self.process_raw_card(raw_card)
+        except StopIteration:
+            print 'Done!'
+
+    def add_card(self, card):
+        self.cards.append(card)
+
+    def process_raw_card(self, raw_card):
+        unicode_card = raw_card.decode('utf-8')
+        stripped = unicode_card.strip()
+        fields = stripped.split('\t')
+        rank, quotient, last_reviewed, question, answer = tuple(fields)
+        last_reviewed_obj = datetime.strptime(last_reviewed, DATE_FORMAT)
+        now = datetime.now()
+        age = now - last_reviewed_obj
+        card = Card(rank, quotient, age, question, answer)
+        print repr(card)
+
+class Coach(object):
+    def __init__(self):
+        self.collections = []
+
+class Session(object):
+    def __init__(self):
         pass
+    
+    def get_date(self):
+        now = datetime.now()
+        print now.strftime(DATE_FORMAT)
+
+
 
 def main():
-    sourcefile = 'freq'
-    vocab_file = read_three_lines_at_a_time(sourcefile)
+    top10 = Collection('top10.tsv')
 
 if __name__ == '__main__':
     main()
